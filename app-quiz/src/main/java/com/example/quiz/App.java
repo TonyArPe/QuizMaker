cee0c4b7af7dec9bc6c0048813f649b2f837ae16
Tony Aragón <tonyaragonp@gmail.com>
2025-04-01 17:24:35 +0200

    Creacion de Test de Generacion de Archivo Formulario

--- app-quiz/src/main/java/com/example/quiz/App.java
+++ app-quiz/src/main/java/com/example/quiz/App.java
@@ -1,13 +1,17 @@
 package com.example.quiz;
 
 /**
- * Hello world!
- *
+ * @author TonyArPe
+ * @version 1.0
+ * @since 2025-04-01
+ * 
+ * @description Programa el cual de un documento de texto, extrae el apartado
+ * donde aparecen los test del temario correspondiente y sincroniza a 
+ * Google Forms para la creacion del test y futura realizacion del mismo
+ * por parte del alumno.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
